249a22021c8e8591b92017e25f933378bc82e308
ArslanHaideriintellect <arslanhaider.iintellect@gmail.com>
2019-12-30 15:49:47 +0500

    build issue solvef

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,11 +1,12 @@
 import ApolloClient from 'apollo-boost';
-let userToken = typeof window !== 'undefined' && localStorage.getItem("token");
+// let userToken = typeof window !== 'undefined' &&  localStorage.getItem("token");
 export const client = new ApolloClient({
+
     uri: 'https://vft2h7l6zrbudbynw462k2ndnu.appsync-api.us-east-1.amazonaws.com/graphql',
     request: operation => {
         operation.setContext({
             headers: {
-                authorization: userToken, //Your Auth token extraction logic
+                authorization: localStorage.getItem("token"), //Your Auth token extraction logic
             },
         });
     },
--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,17 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import {ApolloProvider} from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import fetch from 'isomorphic-fetch';
+
 // import { client } from './client';
 
 
 const WrapRootElement = ({children}) => {
-    let userToken = localStorage && localStorage.getItem("token");
+    // const userToken =  typeof localStorage !=="undefined" && localStorage.getItem("token");
     const client = new ApolloClient({
         uri: 'https://vft2h7l6zrbudbynw462k2ndnu.appsync-api.us-east-1.amazonaws.com/graphql',
         request: operation => {
             operation.setContext({
                 headers: {
-                    authorization: userToken, //Your Auth token extraction logic
+                    authorization: localStorage ? localStorage.getItem("token") : "" , //Your Auth token extraction logic
+                    fetch
                 },
             });
         },
