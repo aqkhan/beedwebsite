8a5234bc8deed034fb3d7eb35c89dbaee3f52fc6
assadakramiintellect <assad@iintellect.co.uk>
2020-01-01 15:49:23 +0500

    more fixes

--- src/components/confirmationAdmin/index.js
+++ src/components/confirmationAdmin/index.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from "react";
 import Style from "./style";
 import {Container} from "react-bootstrap";
 import axios from "axios";
+import Loader from "../../components/loader";
 
 const ConfirmComponent = () => {
     const [success, setSuccuess] = useState(false);
-
+    const [loader, setLoader] = useState(false);
     useEffect(() => {
         let iniwindow = typeof window !== 'undefined' && window;
         let path = iniwindow && iniwindow.location.search;
@@ -16,8 +17,12 @@ const ConfirmComponent = () => {
             {
                 orderId: path
             }
-        axios.post("https://bls1feondl.execute-api.us-east-1.amazonaws.com/dev/v1/user/order/confirm", submirData).then(res => {
+        axios.post("https://bls1feondl.execute-api.us-east-1.amazonaws.com/dev/v1/admin/order/confirm", submirData).then(res => {
+            setLoader(true);
             setSuccuess(true)
+        }).catch(err => {
+            setLoader(true);
+            setSuccuess(false);
         })
 
     }, [])
@@ -25,18 +30,19 @@ const ConfirmComponent = () => {
     return (
         <Container>
             {
-                success &&
-                <div className="details">
-                    <h1>Congratulations!</h1>
-                    <p>Your Order Has Been Confirmed.</p>
-                </div>
-            }
-
-            {
-                !success && <div className="details">
-                    <h1 className="confirmation-soory">Sorry!</h1>
-                    <p>Your Order Could Not Been Confirmed.</p>
-                </div>
+                loader ?
+                    <div>
+                        {
+                            success ?
+                                <div className="details">
+                                    <h1>Congratulations!</h1>
+                                    <p>Your order has been confirmed.</p>
+                                </div> : <div className="details">
+                                <h1 className="confirmation-soory">Sorry!</h1>
+                                <p>Your order could not be confirmed. Please contact your provider</p>
+                            </div>
+                        }
+                    </div> : <Loader/>
             }
             <Style/>
         </Container>
--- src/components/confirmationComponent/index.js
+++ src/components/confirmationComponent/index.js
@@ -2,42 +2,49 @@ import React, {useState, useEffect} from "react";
 import Style from "./style";
 import {Container} from "react-bootstrap";
 import axios from "axios";
+import Loader from "../../components/loader";
 
 const ConfirmComponent = () => {
     const [success, setSuccuess] = useState(false);
-
+    const [loader, setLoader] = useState(false);
     useEffect(() => {
         let iniwindow = typeof window !== 'undefined' && window;
         let path = iniwindow && iniwindow.location.search;
         path = path && path.split("=");
         path = path && path[1];
 
-        let submirData =
-            {
+        let submirData = {
                 orderId: path
-            }
+            };
         axios.post("https://bls1feondl.execute-api.us-east-1.amazonaws.com/dev/v1/user/order/confirm", submirData).then(res => {
+            setLoader(true);
             setSuccuess(true)
+        }).catch(err => {
+            setLoader(true);
+            setSuccuess(false);
         })
 
-    }, [])
+    }, []);
 
     return (
         <Container>
             {
-                success &&
-                <div className="details">
-                    <h1>Congratulations!</h1>
-                    <p>Your Order Has Been Confirmed.</p>
-                </div>
+                loader ?
+                <div>
+                    {
+                        success ?
+                        <div className="details">
+                            <h1>Congratulations!</h1>
+                            <p>Your order has been confirmed.</p>
+                        </div> : <div className="details">
+                        <h1 className="confirmation-soory">Sorry!</h1>
+                        <p>Your order could not be confirmed. Please contact your provider</p>
+                        </div>
+                    }
+                </div> : <Loader/>
             }
 
-            {
-                !success && <div className="details">
-                    <h1 className="confirmation-soory">Sorry!</h1>
-                    <p>Your Order Could Not Been Confirmed.</p>
-                </div>
-            }
+
             <Style/>
         </Container>
     )
